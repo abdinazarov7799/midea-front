fd44b4f5bdf9a81208e65aba227c704b62ba3720
abdinazarov7799 <parolxato@gmail.com>
2025-05-14 01:49:24 +0500

    fix

--- src/modules/tg-web-app/CreateClientPage.jsx
+++ src/modules/tg-web-app/CreateClientPage.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Container from "../../components/Container.jsx";
 import {useTelegram} from "../../hooks/telegram/useTelegram.js";
-import {Button, Form, Input, InputNumber, Select} from "antd";
+import {Button, Checkbox, Form, Input, InputNumber, Select} from "antd";
 import {useTranslation} from "react-i18next";
 import useGetAllQuery from "../../hooks/api/useGetAllQuery.js";
 import {get} from "lodash";
@@ -11,8 +11,9 @@ const CreateClientPage = () => {
     const [form] = Form.useForm();
     const {t} = useTranslation();
     const telegram = useTelegram();
+    const [isLegal, setIsLegal] = React.useState(false);
 
-    const {data,isLoading} = useGetAllQuery({
+    const {data, isLoading} = useGetAllQuery({
         key: 'web-dealers-list',
         url: '/api/web/dealers/get-all',
         params: {
@@ -21,13 +22,13 @@ const CreateClientPage = () => {
             }
         }
     })
-    const {mutate,isLoading:isLoadingCreate} = usePostQuery({})
+    const {mutate, isLoading: isLoadingCreate} = usePostQuery({})
 
     const createClient = (values) => {
         mutate({
             url: "/api/web/clients/create",
-            attributes: values
-        },{
+            attributes: {...values,legal: isLegal}
+        }, {
             onSuccess: () => {
                 telegram.onClose();
             },
@@ -48,28 +49,31 @@ const CreateClientPage = () => {
                         filterOption={(input, option) =>
                             (option?.label ?? '').toLowerCase().includes(input.toLowerCase())}
                         loading={isLoading}
-                        options={get(data,'data.content')?.map((item) => {
+                        options={get(data, 'data.content')?.map((item) => {
                             return {
-                                value: get(item,'id'),
-                                label: get(item,'fullName')
+                                value: get(item, 'id'),
+                                label: get(item, 'fullName')
                             }
                         })}
                     />
                 </Form.Item>
-                <Form.Item name={'name'} rules={[{required:true}]} label={'Name'}>
-                    <Input />
+                <Form.Item name={'name'} rules={[{required: true}]} label={'Name'}>
+                    <Input/>
                 </Form.Item>
-                <Form.Item name={'phone'} rules={[{required:true}]} label={'Phone'}>
-                    <Input />
+                <Form.Item name={'phone'} rules={[{required: true}]} label={'Phone'}>
+                    <Input/>
                 </Form.Item>
-                <Form.Item name={'type'} rules={[{required:true}]} label={'Type'}>
-                    <Input />
+                <Form.Item name={'type'} rules={[{required: true}]} label={'Type'}>
+                    <Select options={[{label: t("ACCOUNTED"), value: 'ACCOUNTED'}, {
+                        label: t("NON_ACCOUNTED"),
+                        value: 'NON_ACCOUNTED'
+                    }]}/>
                 </Form.Item>
-                <Form.Item name={'legal'} rules={[{required:true}]} label={'Legal'}>
-                    <Input />
+                <Form.Item name={'balance'} rules={[{required: true}]} label={'Balance'}>
+                    <InputNumber style={{width: '100%'}}/>
                 </Form.Item>
-                <Form.Item name={'balance'} rules={[{required:true}]} label={'Balance'}>
-                    <InputNumber style={{width:'100%'}} />
+                <Form.Item name={'legal'} label={'Legal'}>
+                    <Checkbox checked={isLegal} onChange={() => setIsLegal(prevState => !prevState)}/>
                 </Form.Item>
                 <Form.Item>
                     <Button type="primary" htmlType="submit" block loading={isLoading || isLoadingCreate}>
