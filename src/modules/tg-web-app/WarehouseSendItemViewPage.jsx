5aeae729ce133f52442ace3511e32a4b1aaec06f
abdinazarov7799 <parolxato@gmail.com>
2025-06-15 23:31:16 +0500

    fix

--- src/modules/tg-web-app/WarehouseSendItemViewPage.jsx
+++ src/modules/tg-web-app/WarehouseSendItemViewPage.jsx
@@ -1,7 +1,7 @@
-import React, { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
-import {Card, Typography, Button, Select, message, Space, Input} from 'antd';
-import { useTranslation } from 'react-i18next';
+import React, {useState, useEffect} from 'react';
+import {useParams} from 'react-router-dom';
+import {Card, Typography, Button, Select, message, Space, Input, Row, Col} from 'antd';
+import {useTranslation} from 'react-i18next';
 import useGetAllQuery from '../../hooks/api/useGetAllQuery';
 import usePutQuery from '../../hooks/api/usePutQuery';
 import usePostQuery from '../../hooks/api/usePostQuery';
@@ -9,47 +9,69 @@ import {get, isEqual} from 'lodash';
 import {ArrowLeftOutlined} from "@ant-design/icons";
 import {useTelegram} from "../../hooks/telegram/useTelegram.js";
 
-const { Text } = Typography;
+const {Text} = Typography;
 
 const WarehouseSendItemViewPage = () => {
-    const { t } = useTranslation();
-    const { id, roleId, userId,dealerId } = useParams();
+    const {t} = useTranslation();
+    const {id, roleId, userId, dealerId} = useParams();
     const [courierId, setCourierId] = useState(null);
     const [formDisabled, setFormDisabled] = useState(false);
     const [isRejecting, setIsRejecting] = useState(false);
     const [comment, setComment] = useState(null);
     const telegram = useTelegram();
+    const [products, setProducts] = useState([]);
 
-    const { data: orderData } = useGetAllQuery({
+    const {data: orderData} = useGetAllQuery({
         key: ['order', id],
         url: `/api/web/orders/get/${id}/${userId}`,
-        params: { params: { roleId } }
+        params: {params: {roleId}}
     });
 
-    const { data: couriersData } = useGetAllQuery({
+    const {data: couriersData} = useGetAllQuery({
         key: ['couriers'],
         url: `/api/web/couriers/get`
     });
 
-    const confirmShipping = usePutQuery({listKeyId: ['order'],hideSuccessToast: true});
-    const linkCourier = usePutQuery({listKeyId: ['order'],hideSuccessToast: true});
+    const {data: sections, isLoading: isLoadingSections} = useGetAllQuery({
+        key: ['sections', userId],
+        url: `/api/web/warehouse-sections/get-reliable/${userId}`
+    })
+
+    const confirmShipping = usePutQuery({listKeyId: ['order'], hideSuccessToast: true});
+    const linkCourier = usePutQuery({listKeyId: ['order'], hideSuccessToast: true});
 
     const order = get(orderData, 'data', {});
     const couriers = get(couriersData, 'data.content', []);
 
     useEffect(() => {
-        if (!isEqual(get(order,'status'),'READY_TO_SHIP')) {
+        if (!isEqual(get(order, 'status'), 'READY_TO_SHIP')) {
             setFormDisabled(true);
-        }else {
+        } else {
             setFormDisabled(false);
         }
     }, [order]);
+
+    useEffect(() => {
+        const allItems = get(orderData, 'data.sectionItems', [])?.flatMap(s => s.items);
+        setProducts(allItems?.map(item => ({sectionId: null, productId: get(item,'product.id'), quantity: get(item,'quantity')})));
+    },[orderData])
+
+    const handleAssignSection = (sectionId, productId) => {
+        const updated = products?.map(item => {
+            if (item.productId === productId) {
+                return { ...item, sectionId };
+            }
+            return item;
+        });
+
+        setProducts(updated);
+    };
 
     const handleCourierAssign = () => {
         if (!courierId) return message.error(t("Iltimos, kuryerni tanlang"));
 
         linkCourier.mutate({
-            url: `/api/web/warehouse-workers/link-courier/${id}/${userId}?courierId=${courierId}&completed=${!get(orderData,'data.deliver')}`,
+            url: `/api/web/warehouse-workers/link-courier/${id}/${userId}?courierId=${courierId}&completed=${!get(orderData, 'data.deliver')}`,
         }, {
             onSuccess: () => {
                 setFormDisabled(true);
@@ -76,45 +98,81 @@ const WarehouseSendItemViewPage = () => {
         });
     }
 
+    console.log(products,'products')
     const handleConfirm = (confirmed) => {
-        confirmShipping.mutate({
-            url: `/api/web/warehouse-workers/confirm-shipping/${id}/${userId}?confirm=${confirmed}`,
-            attributes: {
-                comment,
-                products: [
-                    {
-                        "sectionId": null,
-                        "productId": null,
-                        "quantity": 0
-                    }
-                ]
+        if (confirmed) {
+            const hasMissingSection = products?.some(item => !item?.sectionId);
+
+            if (hasMissingSection) {
+                return message.warning(t("Sectionlar biriktirilmagan"));
             }
-        }, {
-            onSuccess: () => {
-                message.success(
-                    t("Buyurtma chiqarishga tayyorlandi"),
-                    3,
-                    () => telegram.onClose()
-                );
-            }
-        });
+
+            confirmShipping.mutate({
+                url: `/api/web/warehouse-workers/confirm-shipping/${id}/${userId}?confirm=true`,
+                attributes: {
+                    products
+                }
+            }, {
+                onSuccess: () => {
+                    message.success(
+                        t("Buyurtma chiqarishga tayyorlandi"),
+                        3,
+                        () => telegram.onClose()
+                    );
+                }
+            });
+
+        } else {
+            confirmShipping.mutate({
+                url: `/api/web/warehouse-workers/confirm-shipping/${id}/${userId}?confirm=false`,
+                attributes: {
+                    comment
+                }
+            }, {
+                onSuccess: () => {
+                    message.success(
+                        t("Buyurtma chiqarishga tayyorlandi"),
+                        3,
+                        () => telegram.onClose()
+                    );
+                }
+            });
+        }
     };
 
+
     return (
-        <Card title={<Space><Button icon={<ArrowLeftOutlined/>} onClick={() => history.back()} /><Typography.Text>{`${t("Buyurtma raqami")}: #${order?.code || order?.id}`}</Typography.Text></Space>}>
+        <Card title={<Space><Button icon={<ArrowLeftOutlined/>}
+                                    onClick={() => history.back()}/><Typography.Text>{`${t("Buyurtma raqami")}: #${order?.code || order?.id}`}</Typography.Text></Space>}>
             <p><b>{t("Buyurtmachi")}:</b> {order?.client}</p>
             <p><b>{t("Mahsulotlar")}:</b></p>
             {
-                get(order,'sectionItems',[])?.map((item, index) => {
+                get(order, 'sectionItems', [])?.map((item, index) => {
                     return (
                         <Card
                             key={index}
-                            title={`${t("Section")}: ${get(item,'sectionName') ?? '-'}`}
+                            title={`${t("Section")}: ${get(item, 'sectionName') ?? '-'}`}
                             style={{margin: '10px 0'}}
-                            styles={{body: {padding: 8},header: {padding: '0 8px'}}}
+                            styles={{body: {padding: 8}, header: {padding: '0 8px'}}}
                         >
                             {item?.items?.map((item, i) => (
-                                <p key={i}><b>L {item?.product?.model}</b> x {item?.quantity} {t("dona")}</p>
+                                <Row justify={'space-between'} key={i} align={'middle'}>
+                                    <Col span={16}>
+                                        <p><b>L {item?.product?.model}</b> x {item?.quantity} {t("dona")}</p>
+                                    </Col>
+                                    <Col span={8}>
+                                        <Select
+                                            style={{width:'100%'}}
+                                            options={get(sections, 'data', [])?.map((item) =>{
+                                                return {
+                                                    label: get(item, 'name'),
+                                                    value: get(item,'id')
+                                                }
+                                            })}
+                                            onChange={(sectionId) => handleAssignSection(sectionId,item?.product?.id)}
+                                        />
+                                    </Col>
+                                </Row>
                             ))}
                         </Card>
                     )
@@ -133,15 +191,15 @@ const WarehouseSendItemViewPage = () => {
                         placeholder={t("Izoh qoldiring")}
                         value={comment}
                         onChange={(e) => setComment(e.target.value)}
-                        style={{ marginTop: 16 }}
+                        style={{marginTop: 16}}
                     />
                 )
             }
 
             {
-                (isEqual(get(order,'status'),'READY_TO_SHIP') && get(order,'delivery')) && (
+                (isEqual(get(order, 'status'), 'READY_TO_SHIP') && get(order, 'delivery')) && (
                     <>
-                        <p style={{ marginTop: 16 }}><b>{t("Kuryerni tanlang")}:</b></p>
+                        <p style={{marginTop: 16}}><b>{t("Kuryerni tanlang")}:</b></p>
                         <Select
                             placeholder={t("Tanlash")}
                             options={couriers?.map(c => ({
@@ -151,7 +209,7 @@ const WarehouseSendItemViewPage = () => {
                             value={courierId}
                             onChange={val => setCourierId(val)}
                             disabled={formDisabled}
-                            style={{ width: '100%', marginBottom: 12 }}
+                            style={{width: '100%', marginBottom: 12}}
                         />
                         <Button
                             type="primary"
@@ -166,10 +224,10 @@ const WarehouseSendItemViewPage = () => {
             }
 
             {
-                !get(order,'delivery') && (
+                !get(order, 'delivery') && (
                     <Button
                         type="primary"
-                        style={{ marginTop: 16 }}
+                        style={{marginTop: 16}}
                         block
                         onClick={handlePickUp}
                     >
@@ -178,29 +236,29 @@ const WarehouseSendItemViewPage = () => {
                 )
             }
 
-            <Space direction="vertical" style={{width:'100%', marginTop: 80}}>
+            <Space direction="vertical" style={{width: '100%', marginTop: 80}}>
                 <Button
                     type="primary"
                     danger
-                    style={{ marginTop: 16 }}
+                    style={{marginTop: 16}}
                     block
                     onClick={() => {
                         if (isRejecting) {
                             handleConfirm(false)
-                        }else {
+                        } else {
                             setIsRejecting(true)
                         }
                     }}
-                    disabled={!isEqual(get(order,'status'),'CREATED') || (isRejecting && !comment)}
+                    disabled={!isEqual(get(order, 'status'), 'CREATED') || (isRejecting && !comment)}
                 >
                     {t("Buyurtma chiqarishni bekor qilish")}
                 </Button>
                 <Button
                     type="primary"
-                    style={{ marginTop: 16 }}
+                    style={{marginTop: 16}}
                     block
                     onClick={() => handleConfirm(true)}
-                    disabled={!isEqual(get(order,'status'),'CREATED')}
+                    disabled={!isEqual(get(order, 'status'), 'CREATED')}
                 >
                     {t("Buyurtma chiqarishga tayyorligini tasdiqlash")}
                 </Button>
