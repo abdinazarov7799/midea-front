ef13102ee00628596286c7c6e56ca08d6f20bab7
Diyorbek <parolxato@gmail.com>
2025-06-16 22:51:02 +0500

    fix

--- src/services/i18n/index.jsx
+++ src/services/i18n/index.jsx
@@ -11,7 +11,7 @@ const i18config = i18n
     .init({
         lng: storage.get("lang") || config.DEFAULT_APP_LANG,
         fallbackLng: storage.get("lang") || config.DEFAULT_APP_LANG,
-        saveMissing: false,
+        saveMissing: true,
         detection: {
             order: ["localStorage"],
             lookupLocalStorage: "lang",
